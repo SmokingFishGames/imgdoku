350f120e2a513189a0ba62918e1ccc9abba2c5f9
Manic <basbalmanic@gmail.com>
2013-04-06 00:49:40 -0700

    Added pinterest

--- res/scripts/share.js
+++ res/scripts/share.js
@@ -9,17 +9,21 @@ $(document).ready(function() {
 	//	}
 	//}
 	sharedAlbumID = getUrlVars()['h'];
+	//<a data-pin-config="beside" href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F&media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest" data-pin-do="buttonPin" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>
 	$.ajax({
-		url: 'https://api.imgur.com/3/album/' + sharedAlbumID + '/images',
+		url: 'https://api.imgur.com/3/album/' + sharedAlbumID,
 		type: 'GET',
 		beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Client-ID '+imgurClientID);},
 		success: function(data) {
-			if (data.data.length >= 9) {
+			var url = encodeURI(window.location.href);
+			var media = encodeURI('http://i.imgur.com/' + data.data.cover + '.jpg');
+			$('#pinterestHolder').html('<a target="_blank" data-pin-config="beside" href="//pinterest.com/pin/create/button/?url=' + url + '&media=' + media + '&description=Imagedoku album recommendation: ' + data.data.id + '" data-pin-do="buttonPin" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>');
+			if (data.data.images.length >= 9) {
 				for (var i = 0; i < 9; i++) {
-					var imgID = data.data[i].id;
+					var imgID = data.data.images[i].id;
 					imgID += 't';
 					imgID = 'http://i.imgur.com/' + imgID + '.jpg';
-					$('#albumHolder').append('<div class="albumThumbHolder"><a href="' + data.data[i].link + '" target="_blank"><img class="imguralbumthumb" title="' + data.data[i].id + '" src="' + imgID + '" /></a></div>');
+					$('#albumHolder').append('<div class="albumThumbHolder"><a href="' + data.data.images[i].link + '" target="_blank"><img class="imguralbumthumb" title="' + data.data.images[i].id + '" src="' + imgID + '" /></a></div>');
 				}
 			} else {
 				$('#insufficientImagesError').modal();
