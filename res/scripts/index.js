96c2ef94e58f43b28bab14d1f6af4a7df42d4fbd
Manic <basbalmanic@gmail.com>
2013-04-10 20:05:39 -0700

    finalized browser checking

--- res/scripts/index.js
+++ res/scripts/index.js
@@ -431,29 +431,4 @@ $(document).ready(function() {
 	} else {
 		$('#createdPalettes').text('You haven\'t made any image sets yet.');
 	}
-	//IE catchall due to XDR still proving troublesome.
-	if ($.browser.name == 'msie' && $.browser.versionNumber < 10) {
-		$.reject({
-			reject: {
-				safari: false, // Apple Safari
-				chrome: false, // Google Chrome
-				firefox: false, // Mozilla Firefox
-				msie: true, // Microsoft Internet Explorer
-				opera: false, // Opera
-				konqueror: false, // Konqueror (Linux)
-				unknown: false // Everything else
-			},
-			header: 'We do not currently support Internet Explorer <10',
-			paragraph1: 'Due to Internet Explorer versions 9 and below not supporting many of the core technologies crucial to Imagedoku, it is unlikely you\'ll be able to play this game.',
-			paragraph2: 'We strongly suggest you upgrade to a standards-compliant browser:',
-			display: ['chrome', 'firefox', 'safari', 'msie'],
-			browserInfo: {  
-				msie: { // Specifies browser name and image name (browser_konqueror.gif)  
-					text: 'Internet Explorer 10', // Text Link  
-					url: 'http://windows.microsoft.com/en-US/internet-explorer/download-ie' // URL To link to  
-				}
-			},
-			closeCookie: true
-		});
-	}
 });
--- res/scripts/utils.js
+++ res/scripts/utils.js
@@ -5,3 +5,32 @@ function getUrlVars() {
 	});
 	return vars;
 }
+$(document).ready(function() {
+	console.log('checking browser...');
+	console.log($.browser.name + ' ' + $.browser.versionNumber);
+	//IE catchall due to XDR still proving troublesome.
+	if ($.browser.name == 'msie' && $.browser.versionNumber < 10) {
+		$.reject({
+			reject: {
+				safari: false, // Apple Safari
+				chrome: false, // Google Chrome
+				firefox: false, // Mozilla Firefox
+				msie: true, // Microsoft Internet Explorer
+				opera: false, // Opera
+				konqueror: false, // Konqueror (Linux)
+				unknown: false // Everything else
+			},
+			header: 'We do not currently support Internet Explorer <10',
+			paragraph1: 'Due to Internet Explorer versions 9 and below not supporting many of the core technologies crucial to Imagedoku, it is unlikely you\'ll be able to play this game.',
+			paragraph2: 'We strongly suggest you upgrade to a standards-compliant browser:',
+			display: ['chrome', 'firefox', 'safari', 'msie'],
+			browserInfo: {  
+				msie: { // Specifies browser name and image name (browser_konqueror.gif)  
+					text: 'Internet Explorer 10', // Text Link  
+					url: 'http://windows.microsoft.com/en-US/internet-explorer/download-ie' // URL To link to  
+				}
+			},
+			closeCookie: true
+		});
+	}
+});
